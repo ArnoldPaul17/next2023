16ef31ab0f173208a37f56332fc0b8ae67c597e8
ArnoldPaul17 <paularnold17@gmail.com>
2023-07-04 20:12:26 -0300

    forms post

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -5,21 +5,22 @@ export default function Contacto () {
         <>
             <h1>Estás en contacto</h1>
 
-            <form action="" method="" className='formulario'>
+            <form method="post" className='formulario'>
                 <label>
                     Nombre:
-                    <input type="text" />
+                    <input type="text" name="nombre" />
                 </label>
                 <label>
                     Mail:
-                    <input type="email" />
+                    <input type="email" name="email" />
                 </label>
                 <label>
                     Comentarios:
-                    <textarea></textarea>
+                    <textarea name="comentario"></textarea>
                 </label>
 
-                <input type="submit" value="Enviar" />
+                {/*<input type="submit" value="Enviar" />*/}
+                <button type='submit'>Enviar</button>
             </form>
         </>
     )
