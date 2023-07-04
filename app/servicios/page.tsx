254fafe0495635f4936b62285484b6343105689c
ArnoldPaul17 <paularnold17@gmail.com>
2023-07-04 20:24:42 -0300

    mod servicios sumar 1

--- app/servicios/page.tsx
+++ app/servicios/page.tsx
@@ -1,24 +1,18 @@
+'use client';
+
+import { useState } from "react";
+
 export default function Servicios () {
+    const [number, setNumber] = useState(0);
+
     return (
         <>
             <h1>Estás en Servicio</h1>
 
-            <form action="" method="" className='formulario'>
-                <label>
-                    Nombre:
-                    <input type="text" />
-                </label>
-                <label>
-                    Mail:
-                    <input type="email" />
-                </label>
-                <label>
-                    Comentarios:
-                    <textarea></textarea>
-                </label>
-
-                <input type="submit" value="Enviar" />
-            </form>
+            <p>Por ahora vamos {number}</p>
+            <button onClick={() => {setNumber(number + 1)}}>
+                sumar 1 al numero
+            </button>
         </>
     )
 }
